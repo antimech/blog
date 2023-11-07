df79680407ba4280801b51bd09f3a4c6c4cdb229
Artur Gauzer <artur.gauzer@pm.me>
2023-11-07 19:59:58 +0600

    Add user - post relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -18,4 +19,12 @@ class Post extends Model
         'title',
         'body',
     ];
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
