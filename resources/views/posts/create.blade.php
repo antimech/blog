cef1f6e3fa0e5cd8e9b04d12026888a2ab7753bf
Artur Gauzer <artur.gauzer@pm.me>
2023-11-07 20:02:46 +0600

    Make default row count in textarea bigger

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -25,6 +25,7 @@
                                     id="body"
                                     name="body"
                                     class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                                    rows="32"
                                     required
                                 >{{ old('body') }}</textarea>
                                 <x-input-error class="mt-2" :messages="$errors->get('body')" />
