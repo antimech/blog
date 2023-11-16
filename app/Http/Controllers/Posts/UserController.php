71e6478debe06e153eb40a05be9cdff6a3bc0c52
Artur Gauzer <artur.gauzer@pm.me>
2023-11-16 21:53:39 +0600

    Remove unnecessary argument from authorizeResource

--- app/Http/Controllers/Posts/UserController.php
+++ app/Http/Controllers/Posts/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function __construct()
     {
-        $this->authorizeResource(Post::class, 'post');
+        $this->authorizeResource(Post::class);
     }
 
     /**
