223eb218a9af0790a389a931f1caf8150f8f1900
Artur Gauzer <artur.gauzer@pm.me>
2023-12-15 20:14:39 +0600

    Refactor `noscript` notice to a Blade component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,25 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <noscript>
-            <div class="bg-yellow-700">
-                <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
-                    <div class="flex items-center justify-between flex-wrap">
-                        <div class="w-0 flex-1 flex items-center min-w-0">
-                        <span class="flex p-2 rounded-lg bg-yellow-600">
-                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                            </svg>
-                        </span>
-
-                            <p class="ms-3 font-medium text-sm text-gray-900 dark:text-gray-100 truncate">
-                                We're sorry but this page doesn't work properly without JavaScript enabled. Please enable it to continue.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </noscript>
+        <x-noscript />
 
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
